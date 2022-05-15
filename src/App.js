1bbdd1f63ebe3c098cb182aa270af95d64f972e1
Rupak Kumar Patro <56712405+KumarRupak@users.noreply.github.com>
2022-05-15 13:59:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,61 +1,18 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { Login } from './UiComponent/Login';
-import { RegisterOrg } from './UiComponent/RegisterOrg';
-import { SignUp } from './UiComponent/Signup';
-import {Switch,Route} from "react-router-dom";
-import { CreditTest } from './UiComponent/CreditTest';
-import { auth } from './testing/auth';
-import { Home } from './UiComponent/Home';
-import {CustomerHome} from './UiComponent/CustomerHome';
-import { SetPin } from './UiComponent/SetPin';
-import { Profile } from './UiComponent/Profile';
-import { MyCards } from './UiComponent/MyCards';
-import { FundTransfer, PayEmi } from './UiComponent/PayEmi';
-import { TransactionHistory } from './UiComponent/TransactionHistory';
-import { CibilCalculator } from './UiComponent/CibilCalculator';
-import { SetLimit } from './UiComponent/SetLimit';
-import { SmartPay } from './UiComponent/SmartPay';
-import { TransferFund } from './UiComponent/TransferFund';
-import { AdminHome } from './UiComponent/AdminHome';
-import {AdminProfile} from './UiComponent/AdminProfile';
-import { PublishCard } from './UiComponent/PublishCard';
-import { ErrorNotFound } from './ErrorComponent/ErrorNotFound';
-
-
-
-
-
-
+import { DopAccountUpdate } from './ComponentCustomer/DopAccountUpdate';
+import DopViewAccounts from './ComponentCustomer/DopViewAccounts';
+import { Routes, Route, Link,BrowserRouter } from "react-router-dom";
 
 function App() {
-  const isLog=false;
   return (
-      <>
-     { 
-     <Switch>
-        <Route exact path='/' component={Login}/>
-        <Route exact path='/signupcus' component={SignUp}/>
-        <Route exact path='/signuporg' component={RegisterOrg}/>
-        <Route exact path='/auth' component={auth}/>
-        <Route exact path='/cushome' component={Home}/>
-        <Route exact path='/cusprofile' component={CustomerHome}/>
-        <Route exact path='/adminhome' component={AdminHome}/>
-        <Route exact path='/credittest' component={CreditTest}/>
-        <Route exact path='/generatepin' component={SetPin}/>
-        <Route exact path='/cusprof' component={Profile}/>
-        <Route exact path='/adminprof' component={AdminProfile}/>
-        <Route exact path='/mycards' component={MyCards}/>
-        <Route exact path='/payemi' component={PayEmi}/>
-        <Route exact path='/transaction' component={TransactionHistory}/>
-        <Route exact path='/cibil' component={CibilCalculator}/>
-        <Route exact path='/limit' component={SetLimit}/>
-        <Route exact path='/smartpay' component={SmartPay}/>
-        <Route exact path='/transfer' component={TransferFund}/>
-        <Route component={ErrorNotFound}/>
-      </Switch>
-     }
-      </>
+    <div>
+    <Routes>
+      <Route  path='/' element={<DopAccountUpdate/>}></Route>
+      <Route  path='/viewaccount' element={<DopViewAccounts/>}></Route>
+    </Routes>
+   
+    </div>
   );
 }
 
